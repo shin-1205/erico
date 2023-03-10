f263f36aeb9f9050a9b50b3f2392a8ada1213e82
silly <>
2023-03-10 20:32:20 +0800

    ベース作成

---
+++ app/public/wp-content/themes/orico/front-page.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo get_theme_file_uri(); ?>/style.css">
+
+  <title>Document</title>
+</head>
+
+<body>
+
+  <script src="<?php echo get_theme_file_uri(); ?>/js/bundle.js"></script>
+</body>
+
+</html>
