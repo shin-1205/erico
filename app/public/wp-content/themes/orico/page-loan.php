babbc8876175d58b5a786d91072c8d01e3a2a24a
silly <>
2023-03-18 03:26:36 +0800

    初めてのローン　レスポンシブ

--- app/public/wp-content/themes/orico/page-loan.php
+++ app/public/wp-content/themes/orico/page-loan.php
@@ -62,7 +62,7 @@
         </h2>
       </div>
 
-      <div class="Loan__KindInner">
+      <div class="container">
         <div class="Loan__KindInnerTop">
           ローンにはお金の使いみちによって、さまざまな種類があります。
         </div>
@@ -227,6 +227,452 @@
 
     </section>
 
+    <section class="Loan__First">
+
+      <div class="Sec-title">
+        <span><img src="https://www.orico.co.jp/common/images/pict/pict-support-01.svg" alt=""></span>
+        <h2>
+          初めてのローン
+        </h2>
+      </div>
+
+      <div class="container">
+
+        <div class="Loan__FirstPic">
+          <img src="https://www.orico.co.jp/loan/images/img-loan-01.jpg" alt="">
+        </div>
+
+        <div class="Loan__FirstText">
+
+          <p>どんなシーンで使えるの？いろんなローンがあるけど、どれを選んだら良いの？とお迷いの方<br>
+            「ローン」ってあまり詳しく知らない…そんなあなたにローンの基礎知識をご紹介！</p>
+
+        </div>
+
+
+        <div class="Loan__FirstAccordion">
+
+          <div class="Loan__FirstAccordionItem">
+            <div class="nav-open">
+              <span></span>
+              <h3>ローンの基礎知識</h3>
+            </div>
+
+            <nav>
+              <ul>
+
+                <li>
+
+                  <div class="Loan__FirstAccordionItemInner">
+
+                    <div class="LinkLeftRed">
+                      <a href="">ローンってなに？
+                        <span></span>
+                      </a>
+                    </div>
+
+                    <div class="Loan__FirstAccordionItemInnerItem">
+
+                      <div class="Loan__FirstAccordionItemInnerItemLeft">
+
+                        <p>ローン（loan）は「貸付」「融資」と訳され、一般的に「お金を貸す」ことを意味します。つまり、利用者にとっては「お金を借りる」ことです。</p>
+
+                        <div class="LinkArrowRight">
+
+                          <a href="">
+                            <span>
+                              ローンとは？メリットや目的別の種類、返済、金利についても解説
+                            </span>
+                          </a>
+                        </div>
+                      </div>
+
+                      <div class="Loan__FirstAccordionItemInnerItemRight">
+                        <img src="https://www.orico.co.jp/loan/images/img-loan-09.jpg" alt="">
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </li>
+
+
+                <li>
+
+                  <div class="Loan__FirstAccordionItemInner">
+
+                    <div class="LinkLeftRed">
+                      <a href="">どんなシーンで使えるの？
+                        <span></span>
+                      </a>
+                    </div>
+
+                    <div class="Loan__FirstAccordionItemInnerItem">
+
+                      <div class="Loan__FirstAccordionItemInnerItemLeft">
+
+                        <p>
+                          私たちは働き、収入を得て、買い物や貯蓄をします。ただ、結婚やマイホーム、子どもの養育費などで、貯蓄が足りなくなることもあるでしょう。また、突然の出費でお金を用意することも考えられます。そんなときの解決策として、「お金を借りる＝ローンを組む」という手段があります。
+                        </p>
+
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </li>
+
+                <li>
+
+                  <div class="Loan__FirstAccordionItemInner">
+
+                    <div class="LinkLeftRed">
+                      <a href="">カードローンとキャッシングの違いは？
+                        <span></span>
+                      </a>
+                    </div>
+
+                    <div class="Loan__FirstAccordionItemInnerItem">
+
+                      <div class="Loan__FirstAccordionItemInnerItemLeft">
+
+                        <p>
+                          厳密に違いはなく「カードローン」はキャッシング専用カードでキャッシングを行うことを指します。一方の「キャッシング」はクレジットカードでお金を借りたり、カードローンでお金を借りたりなどお金を借りることをさします。
+                        </p>
+
+                        <div class="LinkArrowRight">
+
+                          <a href="">
+                            <span>
+                              キャッシングとは？カードローンとの違いはあるの？
+                            </span>
+                          </a>
+                        </div>
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </li>
+
+                <li>
+
+                  <div class="Loan__FirstAccordionItemInner">
+
+                    <div class="LinkLeftRed">
+                      <a href="">カードローンと目的別ローンの違いは？
+                        <span></span>
+                      </a>
+                    </div>
+
+                    <div class="Loan__FirstAccordionItemInnerItem">
+
+                      <div class="Loan__FirstAccordionItemInnerItemLeft">
+
+                        <h4>カードローン</h4>
+
+                        <div class="Loan__FirstAccordionItemInnerItemLeftCircle">
+                          <span>カードが発行される。</span>
+                          <span>ご利用可能枠の範囲で何度でも借入れができる。</span>
+                          <span>いざという時の備えになる。</span>
+                        </div>
+
+                        <h4>目的別ローン</h4>
+
+                        <div class="Loan__FirstAccordionItemInnerItemLeftCircle">
+                          <span>カードは発行されない。</span>
+                          <span>1度にまとまった額を借入れできる。</span>
+                          <span>お金の使いみちが決まっているときに便利。</span>
+                        </div>
+
+                        <div class="LinkArrowRight">
+
+                          <a href="">
+                            <span>
+                              カードローンとは？使い方やおすすめのカードローンをご紹介！
+                            </span>
+                          </a>
+                        </div>
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </li>
+
+              </ul>
+            </nav>
+          </div>
+
+          <!-- Item2 -->
+
+          <div class="Loan__FirstAccordionItem">
+            <div class="nav-open">
+              <span></span>
+              <h3>ローンの選び方</h3>
+              <p>ローンにはお金の使いみちによって、さまざまな種類があります</p>
+            </div>
+
+            <nav>
+              <ul>
+
+                <li>
+
+                  <div class="Loan__FirstAccordionItemInner">
+
+                    <div class="LinkLeftRed">
+                      <a href="">キャッシング
+                        <span></span>
+                      </a>
+                    </div>
+
+                    <div class="Loan__FirstAccordionItemInnerItem">
+
+
+                      <div class="Loan__FirstAccordionItemInnerItemRight SPOff">
+                        <img src="https://www.orico.co.jp/loan/images/img-loan-10.jpg" alt="">
+                      </div>
+
+                      <div class="Loan__FirstAccordionItemInnerItemLeft">
+
+                        <p>お手持ちのオリコカードでご利用いただけるキャッシングサービス※です。</p>
+
+                        <h5><span>キャッシング枠を設定されている場合のみとなります。</span></h5>
+
+                        <div class="Loan__FirstAccordionItemInnerItemLeftCircle">
+                          <span>キャッシングサービスはこちら</span>
+                          <span>ご利用枠の設定方法はこちら</span>
+                        </div>
+                      </div>
+
+                      <div class="Loan__FirstAccordionItemInnerItemRight SPON">
+                        <img src="https://www.orico.co.jp/loan/images/img-loan-10.jpg" alt="">
+                      </div>
+
+
+                    </div>
+
+                  </div>
+
+                </li>
+
+
+                <li>
+
+                  <div class="Loan__FirstAccordionItemInner">
+
+                    <div class="LinkLeftRed">
+                      <a href="">カードローン
+                        <span></span>
+                      </a>
+                    </div>
+
+                    <div class="Loan__FirstAccordionItemInnerItem">
+
+                      <div class="Loan__FirstAccordionItemInnerItemRight">
+                        <img src="https://www.orico.co.jp/loan/images/img-loan-11.jpg" alt="">
+                      </div>
+
+                      <div class="Loan__FirstAccordionItemInnerItemLeft">
+
+                        <h6>
+                          必要な時に必要な分だけ借りられるカードローン
+                        </h6>
+
+                        <p>
+                          カードローンは、ご利用可能枠の範囲内であればATMやインターネットを利用して自由に何度でもお金をお借入れ・ご返済ができる、比較的小額の個人向け融資です。<br>
+                          <br>
+                          今すぐお金が必要でなくても、急な出費や万が一のアクシデントに備えて、カードだけ作っておけるというのも、カードローンの便利なポイントです。
+                        </p>
+
+                      </div>
+
+                    </div>
+
+                    <div class="Loan__FirstAccordionItemInnerBottom">
+
+                      <h3>
+                        カードローンの特長
+                      </h3>
+
+                      <div class="Loan__FirstAccordionItemInnerBottomBox">
+
+                        <div class="Loan__FirstAccordionItemInnerBottomBoxInner">
+
+                          <h4>特徴1</h4>
+
+                          <div class="Loan__FirstAccordionItemInnerBottomBoxInnerBottom">
+                            <p>担保・保証人が不要</p>
+
+                            <span>必要書類が少なく、審査が早い。</span>
+                          </div>
+
+                        </div>
+
+                        <div class="Loan__FirstAccordionItemInnerBottomBoxInner">
+
+                          <h4>特徴1</h4>
+
+                          <div class="Loan__FirstAccordionItemInnerBottomBoxInnerBottom">
+                            <p>担保・保証人が不要</p>
+
+                            <span>必要書類が少なく、審査が早い。</span>
+                          </div>
+
+                        </div>
+
+                        <div class="Loan__FirstAccordionItemInnerBottomBoxInner">
+
+                          <h4>特徴1</h4>
+
+                          <div class="Loan__FirstAccordionItemInnerBottomBoxInnerBottom">
+                            <p>担保・保証人が不要</p>
+
+                            <span>必要書類が少なく、審査が早い。</span>
+                          </div>
+
+                        </div>
+
+                      </div>
+
+                      <div class="Loan__FirstAccordionItemInnerItemLeft">
+
+
+                        <div class="LinkArrowRight">
+
+                          <a href="">
+                            <span>
+                              オリコのカードローンについてもっと詳しく
+                            </span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+
+                </li>
+
+
+
+                <li>
+
+                  <div class="Loan__FirstAccordionItemInner">
+
+                    <div class="LinkLeftRed">
+                      <a href="">ローン（提携ローン・目的ローン・ビジネスローン）
+                        <span></span>
+                      </a>
+                    </div>
+
+                    <div class="Loan__FirstAccordionItemInnerItem">
+
+                      <div class="Loan__FirstAccordionItemInnerItemRight">
+                        <img src="https://www.orico.co.jp/loan/images/img-loan-12.jpg" alt="">
+                      </div>
+
+                      <div class="Loan__FirstAccordionItemInnerItemLeft">
+
+                        <h6>
+                          あなたのライフプランをサポートするローン
+                        </h6>
+
+                        <p>
+                          車の購入費用、学費、リフォーム費用、挙式費用など予め目的が決まっているときに便利なローンです。
+                        </p>
+
+                      </div>
+
+                    </div>
+
+                    <div class="Loan__FirstAccordionItemInnerBottom">
+
+                      <h3>
+                        各ローンの特長
+                      </h3>
+
+                      <div class="Loan__FirstAccordionItemInnerBottomBox">
+
+                        <div class="Loan__FirstAccordionItemInnerBottomBoxInner">
+
+                          <h4>提携ローン</h4>
+
+                          <div class="Loan__FirstAccordionItemInnerBottomBoxInnerBottom">
+                            <p>オリコの提携加盟店での<br>
+                              お買い物を安心・便利に</p>
+
+                            <span>あらゆる商品の購入費用を多様なお支払方法でサポートいたします。</span>
+                          </div>
+
+                        </div>
+
+                        <div class="Loan__FirstAccordionItemInnerBottomBoxInner">
+
+                          <h4>目的ローン</h4>
+
+                          <div class="Loan__FirstAccordionItemInnerBottomBoxInnerBottom">
+                            <p>目的別に必要な資金をご融資</p>
+
+                            <span>ご指定された目的内であれば、自由に商品のご購入などにお使いいただけます。</span>
+                          </div>
+
+                        </div>
+
+                        <div class="Loan__FirstAccordionItemInnerBottomBoxInner">
+
+                          <h4>ビジネスローン</h4>
+
+                          <div class="Loan__FirstAccordionItemInnerBottomBoxInnerBottom">
+                            <p>事業主さまのビジネスを<br>
+                              もっと幅広く</p>
+
+                            <span>法人・個人事業主のお客さまのさまざな資金ニーズをサポートいたします。</span>
+                          </div>
+
+                        </div>
+
+                      </div>
+
+                      <div class="Loan__FirstAccordionItemInnerItemLeft">
+
+
+                        <div class="TextLeftCircle">
+                          <a href="">
+                            <span>提携ローンについてもっと詳しく</span>
+                          </a>
+                          <a href="">
+                            <span>目的ローンについてもっと詳しく</span>
+                          </a>
+                          <a href="">
+                            <span>ビジネスローンについてもっと詳しく
+                            </span>
+                            <img src="https://www.orico.co.jp/common/images/icon/icon-window-01.svg" alt="">
+                          </a>
+
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+
+                </li>
+
+
+              </ul>
+            </nav>
+          </div>
+
+
+        </div>
+
+
+      </div>
+
+    </section>
 
   </div>
 
