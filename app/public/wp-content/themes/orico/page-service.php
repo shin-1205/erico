a22a872a2e1cc6e0bfe4b0566326009650fc803f
silly <>
2023-03-19 04:38:03 +0800

    お客さまサービス　ベース作成

--- app/public/wp-content/themes/orico/page-service.php
+++ app/public/wp-content/themes/orico/page-service.php
@@ -8,7 +8,7 @@
 
   <!-- loan Inner start -->
 
-  <div class="Loan">
+  <div class="Service">
 
     <section class="PageTitle">
       <li>
@@ -52,6 +52,591 @@
 
       </div>
     </section>
+
+    <section class="Service__Pay">
+
+      <div class="Sec-title">
+        <span><img src="https://www.orico.co.jp/common/images/pict/pict-card-02.svg" alt=""></span>
+        <h2>
+          〇〇別に見るあなたにぴったりのカード
+        </h2>
+      </div>
+
+      <div class="container">
+
+        <div class="Service__PayText">
+          <p>
+            どんなカードを選べばよいかわからない、そんなあなたにはオリコがおすすめのカードをご紹介いたします。
+          </p>
+        </div>
+
+        <div class="Loan__Question">
+
+
+          <div class="Loan__QuestionInner">
+
+            <div class="Loan__QuestionInnerLeft">
+
+              <h2><span>職業や世代から選ぶ</span></h2>
+
+              <a href="">
+                <span>詳しくはこちら</span>
+              </a>
+
+            </div>
+
+            <div class="Loan__QuestionInnerRight">
+
+              <h2><span>クレジット辞典</span></h2>
+
+              <a href="">
+                <span>詳しくはこちら</span>
+              </a>
+
+            </div>
+
+          </div>
+
+
+        </div>
+
+        <div class="Service__PayBtn">
+          <div class="btn">
+            <a href="#">
+              あらゆるお支払いについて
+              <img src="https://www.orico.co.jp/common/images/icon/icon-window-01.svg" alt="">
+            </a>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+    <!-- 1 -->
+
+    <section class="Loan__Already">
+
+      <div class="Sec-title">
+        <span><img src="https://www.orico.co.jp/common/images/pict/pict-important-01.svg" alt=""></span>
+        <h2>
+          トラブル・お困りのお客さま
+        </h2>
+      </div>
+
+      <div class="container">
+
+        <div class="Loan__AlreadyCard">
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-02.svg" alt="">
+
+              <p>カードの紛失・盗難にあった</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-03.svg" alt="">
+
+              <p>提携ATM/覚えのない明細がある</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-04.svg" alt="">
+
+              <p>カードの暗証番号を忘れた</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-05.svg" alt="">
+
+              <p>eオリコサービスID･パスワードを忘れた</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-pay-03.svg" alt="">
+
+              <p>入金が間に合わなかった</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-07.svg" alt="">
+
+              <p>SMS(確認コード)が届かない</p>
+
+            </a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+
+
+    <section class="Service__Pay">
+
+      <div class="Sec-title">
+        <span><img src="https://www.orico.co.jp/common/images/pict/pict-contact-01.svg" alt=""></span>
+        <h2>
+          お困りのお客さま
+        </h2>
+      </div>
+
+      <div class="container">
+
+        <div class="Service__PayText">
+          <p>
+            オリコのクレジットカードはさまざまなお支払いにご利用いただけます。
+          </p>
+        </div>
+
+        <div class="Service__PayCard">
+          <ul class="Service__PayCardList">
+
+            <li class="Loan__HowCardListItem">
+              <a href="">
+
+                <div class="Loan__HowCardListItemTop">
+                  <div class="Loan__HowCardListItemTopPic">
+                    <img src="https://www.orico.co.jp/common/images/pict/pict-pay-03.svg" alt="">
+                  </div>
+                  <h3>月々のお支払い</h3>
+
+                </div>
+
+                <div class="Loan__HowCardListItemBot">
+
+                  <ul>
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>携帯・固定電話・電報</span>
+                        </a>
+                      </div>
+                    </li>
+
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>電気・ガス・水道</span>
+                        </a>
+                      </div>
+                    </li>
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>新聞・放送</span>
+                        </a>
+                      </div>
+                    </li>
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>家賃</span>
+                        </a>
+                      </div>
+                    </li>
+
+
+                  </ul>
+
+                </div>
+
+
+              </a>
+            </li>
+
+            <li class="Loan__HowCardListItem">
+              <a href="">
+
+                <div class="Loan__HowCardListItemTop">
+                  <div class="Loan__HowCardListItemTopPic">
+                    <img src="https://www.orico.co.jp/common/images/pict/pict-contract-01.svg" alt="">
+                  </div>
+                  <h3>税金のお支払い</h3>
+
+                </div>
+
+                <div class="Loan__HowCardListItemBot">
+
+                  <ul>
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>ふるさと納税</span>
+                        </a>
+                      </div>
+                    </li>
+
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>自動車税</span>
+                        </a>
+                      </div>
+                    </li>
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>住民税</span>
+                        </a>
+                      </div>
+                    </li>
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>固定資産税</span>
+                        </a>
+                      </div>
+                    </li>
+
+                  </ul>
+
+                </div>
+
+
+              </a>
+            </li>
+
+            <li class="Loan__HowCardListItem">
+              <a href="">
+
+                <div class="Loan__HowCardListItemTop">
+                  <div class="Loan__HowCardListItemTopPic">
+                    <img src="https://www.orico.co.jp/common/images/pict/pict-insurance-01.svg" alt="">
+                  </div>
+                  <h3>保険のお支払い</h3>
+
+                </div>
+
+                <div class="Loan__HowCardListItemBot">
+
+                  <ul>
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>生命保険・損害保険</span>
+                        </a>
+                      </div>
+                    </li>
+
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>公的保険・年金</span>
+                        </a>
+                      </div>
+                    </li>
+
+                  </ul>
+
+                </div>
+
+
+              </a>
+            </li>
+
+            <li class="Loan__HowCardListItem">
+              <a href="">
+
+                <div class="Loan__HowCardListItemTop">
+                  <div class="Loan__HowCardListItemTopPic">
+                    <img src="https://www.orico.co.jp/common/images/pict/pict-point-02.svg" alt="">
+                  </div>
+                  <h3>オリコカードでおトクにご利用できるお支払い</h3>
+
+                </div>
+
+                <div class="Loan__HowCardListItemBot">
+
+                  <ul>
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>国内旅行</span>
+                        </a>
+                      </div>
+                    </li>
+
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>海外旅行</span>
+                        </a>
+                      </div>
+                    </li>
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>ネットショッピング（オリコモール）</span>
+                        </a>
+                      </div>
+                    </li>
+
+                    <li>
+                      <div class="TextLeftCircle">
+                        <a href="">
+                          <span>引越・お片づけ</span>
+                        </a>
+                      </div>
+                    </li>
+
+                  </ul>
+
+                </div>
+
+
+              </a>
+            </li>
+
+          </ul>
+        </div>
+
+        <div class="Service__PayBtn">
+          <div class="btn">
+            <a href="#">
+              あらゆるお支払いについて
+              <img src="https://www.orico.co.jp/common/images/icon/icon-window-01.svg" alt="">
+            </a>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+
+    <!-- Support -->
+
+    <section class="Customer__Support">
+
+      <div class="Loan__Already">
+
+        <div class="Sec-title">
+          <span><img src="https://www.orico.co.jp/common/images/pict/pict-contact-01.svg" alt=""></span>
+          <h2>
+            お困りのお客さま
+          </h2>
+        </div>
+
+        <div class="container">
+
+          <h3>
+            日々の生活の中で賢くクレジットカードを使いたい方、新たにクレジットカードのお申込みを検討されている方に耳寄りな情報をご紹介します。
+          </h3>
+
+          <div class="LinkLeftRed">
+            <a href="">
+              ご注意ください
+              <span></span>
+            </a>
+          </div>
+
+          <ul>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>当社名及び当社の類似名称を名乗る金融業者</span>
+                </a>
+              </div>
+            </li>
+
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>盗難スリ</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>「日本クレジット協会」の名称を使用した詐欺行為にご注意ください！</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>架空請求</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>警察・カード会社を装う電話にご注意ください！</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>悪徳商法</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>商品代金全額返金の悪質手口にご注意ください！</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>フィッシング</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>スキミング</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>ショッピング枠の現金化</span>
+                </a>
+              </div>
+            </li>
+
+          </ul>
+
+          <div class="Service__PayBtn">
+            <div class="btn">
+              <a href="#">
+                あらゆるお支払いについて
+                <img src="https://www.orico.co.jp/common/images/icon/icon-window-01.svg" alt="">
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+
+
+      </div>
+
+
+    </section>
+
+
+    <section class="Customer__Support">
+
+      <div class="Loan__Already">
+
+        <div class="Sec-title">
+          <span><img src="https://www.orico.co.jp/common/images/pict/pict-contact-01.svg" alt=""></span>
+          <h2>
+            お困りのお客さま
+          </h2>
+        </div>
+
+        <div class="container">
+
+          <div class="LinkLeftRed">
+            <a href="">
+              お問合せ窓口
+              <span></span>
+            </a>
+          </div>
+
+          <div class="Service__PayBtn">
+            <div class="btn">
+              <a href="#">
+                あらゆるお支払いについて
+                <img src="https://www.orico.co.jp/common/images/icon/icon-window-01.svg" alt="">
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+
+
+      </div>
+
+
+    </section>
+
+
+    <div class="container">
+      <div class="returnTop">
+        <div class="wrapper">
+          <div class="LinkTextTop">
+            <a href="#top">
+              <span>
+                ページトップ
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
 
   </div>
