5819efd08c4423d917c03b3c6ab437fe4c94d6ec
silly <>
2023-03-19 03:19:49 +0800

    お客さまサポート　終了

--- app/public/wp-content/themes/orico/page-customer.php
+++ app/public/wp-content/themes/orico/page-customer.php
@@ -6,9 +6,9 @@
 
   <?php get_sidebar();?>
 
-  <!-- loan Inner start -->
+  <!-- Customer Inner start -->
 
-  <div class="Loan">
+  <div class="Customer">
 
     <section class="PageTitle">
       <li>
@@ -40,44 +40,46 @@
           <img src="https://www.orico.co.jp/common/images/hdg/hdg-support-01.jpg" alt="">
         </div>
 
-        <section class="NewsArea">
+        <div class="container">
+          <div class="Headup">
 
-          <div class="Newswrap">
+            <div class="Headup__Inner">
 
-            <div class="NewsArea__title">
-              <img src="https://www.orico.co.jp/common/images/pict/pict-important-01.svg" alt="">
-              <h2>重要なお知らせ</h2>
-            </div>
+              <div class="Headup__InnerTitle">
+                <img src="https://www.orico.co.jp/common/images/pict/pict-important-01.svg" alt="">
+                <h2>重要なお知らせ</h2>
+              </div>
 
-            <div class="NewsArea__list">
+              <div class="Headup__InnerList">
 
-              <dl class="NewsArea__listItem">
-                <dt>
-                  2023年02月27日
-                </dt>
-                <dd>
-                  <a href="#">
-                    オリコをカルタフィッシングサイトの発生について
-                  </a>
-                </dd>
-              </dl>
+                <dl class="Headup__InnerListItem">
+                  <dt>
+                    2023年02月27日
+                  </dt>
+                  <dd>
+                    <a href="#">
+                      オリコをカルタフィッシングサイトの発生について
+                    </a>
+                  </dd>
+                </dl>
 
-              <dl class="NewsArea__listItem">
-                <dt>
-                  2023年02月27日
-                </dt>
-                <dd>
-                  <a href="#">
-                    令和4年12月17日からの大雪により被害を受けられた地域の皆さまへ
-                  </a>
-                </dd>
-              </dl>
+                <dl class="Headup__InnerListItem">
+                  <dt>
+                    2023年02月27日
+                  </dt>
+                  <dd>
+                    <a href="#">
+                      令和4年12月17日からの大雪により被害を受けられた地域の皆さまへ
+                    </a>
+                  </dd>
+                </dl>
+
+              </div>
 
             </div>
 
           </div>
-
-        </section>
+        </div>
 
         <div class="pageLink">
           <ul>
@@ -91,10 +93,664 @@
       </div>
     </section>
 
+    <!-- 1 -->
+
+    <section class="Loan__Already">
+
+      <div class="Sec-title">
+        <span><img src="https://www.orico.co.jp/common/images/pict/pict-important-01.svg" alt=""></span>
+        <h2>
+          トラブル・お困りのお客さま
+        </h2>
+      </div>
+
+      <div class="container">
+
+        <div class="Loan__AlreadyCard">
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-02.svg" alt="">
+
+              <p>カードの紛失・盗難にあった</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-03.svg" alt="">
+
+              <p>提携ATM/覚えのない明細がある</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-04.svg" alt="">
+
+              <p>カードの暗証番号を忘れた</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-05.svg" alt="">
+
+              <p>eオリコサービスID･パスワードを忘れた</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-pay-03.svg" alt="">
+
+              <p>入金が間に合わなかった</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-07.svg" alt="">
+
+              <p>SMS(確認コード)が届かない</p>
+
+            </a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+    <!-- 2 -->
+
+    <section class="Loan__Already">
+
+      <div class="Sec-title">
+        <span><img src="https://www.orico.co.jp/common/images/pict/pict-procedure-01.svg" alt=""></span>
+        <h2>
+          ご用件から探す
+        </h2>
+      </div>
+
+      <div class="container">
+
+        <div class="Loan__AlreadyCard">
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-15.svg" alt="">
+
+              <p>引落口座の登録・変更</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-10.svg" alt="">
+
+              <p>ご利用明細・お支払い</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-11.svg" alt="">
+
+              <p>お客さま情報の変更</p>
+
+            </a>
+
+          </div>
+
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-12.svg" alt="">
+
+              <p>カードのご利用</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-13.svg" alt="">
+
+              <p>カードのお申込み・更新・解約</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-14.svg" alt="">
+
+              <p>リボ・分割払い</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-09.svg" alt="">
+
+              <p>キャッシング</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-support-16.svg" alt="">
+
+              <p>クレジット契約のクーリング・オフ</p>
+
+            </a>
+
+          </div>
+
+          <div class="Loan__AlreadyCardItem">
+
+            <a href="">
+
+              <img src="https://www.orico.co.jp/common/images/pict/pict-apply-02.svg" alt="">
+
+              <p>各種証明書のWeb申請</p>
+
+            </a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+
+    <!-- Question -->
+
+    <section class="Customer__Question">
+
+
+      <div class="Sec-title">
+        <span><img src="https://www.orico.co.jp/common/images/pict/pict-info-02.svg" alt=""></span>
+        <h2>
+          よくあるご質問
+        </h2>
+      </div>
+
+      <div class="container">
+
+        <div class="LinkLeftRed">
+          <a href="">
+            よく見られているご質問
+            <span></span>
+          </a>
+        </div>
+
+        <ul>
+
+          <li>
+            <a href="">
+              <span>Q</span>
+              <p>eオリコサービスのID・パスワードを忘れてしまいました。</p>
+              <img src="https://www.orico.co.jp/common/images/icon/icon-window-01.svg" alt="">
+            </a>
+          </li>
+
+
+          <li>
+            <a href="">
+              <span>Q</span>
+              <p>SMS（確認コード）が届きません。</p>
+              <img src="https://www.orico.co.jp/common/images/icon/icon-window-01.svg" alt="">
+            </a>
+          </li>
+
+          <li>
+            <a href="">
+              <span>Q</span>
+              <p>可能枠一杯に利用している場合いつから利用できるようになりますか？</p>
+              <img src="https://www.orico.co.jp/common/images/icon/icon-window-01.svg" alt="">
+            </a>
+          </li>
+
+          <li>
+            <a href="">
+              <span>Q</span>
+              <p>利用金額の一部または全額を繰上返済することはできますか？</p>
+              <img src="https://www.orico.co.jp/common/images/icon/icon-window-01.svg" alt="">
+            </a>
+          </li>
+
+          <li>
+            <a href="">
+              <span>Q</span>
+              <p>口座引落日は何日ですか？</p>
+              <img src="https://www.orico.co.jp/common/images/icon/icon-window-01.svg" alt="">
+            </a>
+          </li>
+
+        </ul>
+
+
+        <div class="Customer__QuestionSearch">
+
+          <div class="Customer__QuestionSearchTop">
+
+            <h3>キーワード検索</h3>
+
+            <div class="Customer__QuestionSearchTopBox">
+
+              <div class="Customer__QuestionSearchTopBoxLeft">
+
+                <input type="text">
+                <img src="https://www.orico.co.jp/common/images/icon/icon-search-01.svg" alt="">
+
+              </div>
+              <div class="Customer__QuestionSearchTopBoxRight">
+                <a href="">
+                  検索
+                </a>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="Customer__QuestionSearchBottom">
+
+            <div class="Customer__QuestionSearchBottomLeft">
+              <h3>
+                注目キーワード：
+              </h3>
+            </div>
+
+            <div class="Customer__QuestionSearchBottomRight">
+              <ul>
+                <li>
+                  <a href="">
+                    入会
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    eオリコサービス
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    オリコポイント
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    暮らスマイル
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    カードローン
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    ETCカード
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    プリペイドカード
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    解約
+                  </a>
+                </li>
+
+              </ul>
+
+            </div>
+
+          </div>
+
+
+        </div>
+
+        <div class="Customer__QuestionBtn">
+          <div class="btn">
+            <a href="#">
+              すべてのご質問をみる
+              <img src="https://www.orico.co.jp/common/images/icon/icon-window-01.svg" alt="">
+            </a>
+          </div>
+
+        </div>
+
+
+    </section>
+
+
+    <!-- Support -->
+
+    <section class="Customer__Support">
+
+      <div class="Loan__Already">
+
+        <div class="Sec-title">
+          <span><img src="https://www.orico.co.jp/common/images/pict/pict-contact-01.svg" alt=""></span>
+          <h2>
+            お困りのお客さま
+          </h2>
+        </div>
+
+        <div class="container">
+
+          <div class="LinkLeftRed">
+            <a href="">
+              お問合せ窓口
+              <span></span>
+            </a>
+          </div>
+
+          <div class="Loan__AlreadyCard">
+
+            <div class="Loan__AlreadyCardItem">
+
+              <a href="">
+
+                <img src="https://www.orico.co.jp/common/images/pict/pict-mail-02.svg" alt="">
+
+                <p>メールでのお問合せはこちら</p>
+
+              </a>
+
+            </div>
+
+            <div class="Loan__AlreadyCardItem">
+
+              <a href="">
+
+                <img src="https://www.orico.co.jp/common/images/pict/pict-contact-04.svg" alt="">
+
+                <p>お電話でのお問い合わせはこちら</p>
+
+              </a>
+
+            </div>
+
+            <div class="Loan__AlreadyCardItem">
+
+              <a href="">
+
+                <img src="https://www.orico.co.jp/common/images/pict/pict-contact-03.svg" alt="">
+
+                <p>オリコテレホンサービス</p>
+
+              </a>
+
+            </div>
+
+          </div>
+
+
+          <div class="LinkLeftRed">
+            <a href="">
+              ご注意ください
+              <span></span>
+            </a>
+          </div>
+
+          <ul>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>当社名及び当社の類似名称を名乗る金融業者</span>
+                </a>
+              </div>
+            </li>
+
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>盗難スリ</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>「日本クレジット協会」の名称を使用した詐欺行為にご注意ください！</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>架空請求</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>警察・カード会社を装う電話にご注意ください！</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>悪徳商法</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>商品代金全額返金の悪質手口にご注意ください！</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>フィッシング</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>スキミング</span>
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div class="TextLeftCircle">
+                <a href="">
+                  <span>ショッピング枠の現金化</span>
+                </a>
+              </div>
+            </li>
+
+          </ul>
+
+          <article>
+            <div class="LinkLeftRed">
+              <a href="">
+                成人を迎えられた18歳、19歳の方へ
+                <span></span>
+              </a>
+            </div>
+
+            <div class="Box">
+
+              <div class="Left">
+
+                <h3>クレジットの正しい知識と使い方を学びましょう</h3>
+                <p>
+                  2022年4月1日から成年年齢が引き下げられ、18歳からクレジットカードや一部ローンの契約が一人でできるようになりました。契約をするその前に！消費者トラブルに巻き込まれないために正しい知識を身に付けましょう。
+                </p>
+
+                <div class="LinkTextArrow">
+
+                  <a href="">
+                    <span>
+                      「18歳になったら...何ができる？」を見る
+                    </span>
+                  </a>
+
+                </div>
+
+              </div>
+
+              <div class="Right">
+
+                <img src="https://www.orico.co.jp/common/images/bnr/other/lp-enlighten-01-560x329.png" alt="">
+
+              </div>
+
+            </div>
+
+          </article>
+
+          <article>
+            <div class="LinkLeftRed">
+              <a href="">
+                オリコの取組み
+                <span></span>
+              </a>
+            </div>
+
+
+            <ul>
+
+              <li>
+                <div class="TextLeftCircle">
+                  <a href="">
+                    <span>お客さまの声にお応えして</span>
+                  </a>
+                </div>
+              </li>
+
+
+              <li>
+                <div class="TextLeftCircle">
+                  <a href="">
+                    <span>カード不正使用防止に向けた当社取組みについて</span>
+                  </a>
+                </div>
+              </li>
+
+              <li>
+                <div class="TextLeftCircle">
+                  <a href="">
+                    <span>不正検知システムの導入</span>
+                  </a>
+                </div>
+              </li>
+
+              <li>
+                <div class="TextLeftCircle">
+                  <a href="">
+                    <span>ご本人さま確認を実施しています</span>
+                  </a>
+                </div>
+              </li>
+
+            </ul>
+
+
+
+          </article>
+
+        </div>
+
+      </div>
+
+    </section>
+
+
+    <div class="container">
+      <div class="returnTop">
+        <div class="wrapper">
+          <div class="LinkTextTop">
+            <a href="#top">
+              <span>
+                ページトップ
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
 
   </div>
 
-  <!-- loan Inner finish -->
+  <!-- Customer Inner finish -->
 
 </div>
 
